021b947dd66d9fb33ef1f61c44ef46234a0abe9e
Shindeok Kang <heavymery@gmail.com>
2015-05-30 15:28:50 +0900

    Disable jsHint watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,13 +33,13 @@ module.exports = function (grunt) {
         files: ['bower.json'],
         tasks: ['wiredep']
       },
-      js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all'],
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        }
-      },
+//       js: {
+//         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+//         tasks: ['newer:jshint:all'],
+//         options: {
+//           livereload: '<%= connect.options.livereload %>'
+//         }
+//       },
       jsTest: {
         files: ['test/spec/{,*/}*.js'],
         tasks: ['newer:jshint:test', 'karma']
@@ -57,6 +57,7 @@ module.exports = function (grunt) {
         },
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/scripts/{,*/}*.js',
           '.tmp/styles/{,*/}*.css',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
         ]
