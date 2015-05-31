1826b80f39ab960abbea1f824e85e4bd30eb96fa
Shindeok Kang <heavymery@gmail.com>
2015-05-31 23:04:13 +0900

    Fix sample route

---
+++ app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAngularGeneratorApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the myAngularGeneratorApp
+ */
+angular.module('myAngularGeneratorApp')
+  .controller('ContactCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
